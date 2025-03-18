63a6e8cb34eff999bd5a56c1aceab3c40ebcbdff
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-18 10:44:43 -0300

    feat: add Entity Pagamento, update pom.xml

---
+++ ms-pagamento/src/main/java/com/github/cidarosa/ms_pagamento/entity/Pagamento.java
@@ -0,0 +1,35 @@
+package com.github.cidarosa.ms_pagamento.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+
+@Entity
+@Table(name = "tb_pagamento")
+public class Pagamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private BigDecimal valor;
+    private String nome;
+    private String numeroDoCartao;     // xxxx xxxx xxxx xxxx
+    private String Validade;           // MM/AA
+    private String codigoDeSeguranca;  // xxx
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status;
+    @Column(nullable = false)
+    private Long pedidoId;
+    @Column(nullable = false)
+    private Long formaDePagamentoId;   // 1 - dinheiro | 2 - cartão | 3 - pix
+
+}
