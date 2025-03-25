085a627fc38a57fca2a7184f04ac7d233500f345
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-25 11:15:42 -0300

    test: repository - create pagamento

--- ms-pagamento/src/test/java/com/github/cidarosa/ms_pagamento/repository/PagamentoRepositoryTest.java
+++ ms-pagamento/src/test/java/com/github/cidarosa/ms_pagamento/repository/PagamentoRepositoryTest.java
@@ -1,8 +1,10 @@
 package com.github.cidarosa.ms_pagamento.repository;
 
 import com.github.cidarosa.ms_pagamento.entity.Pagamento;
+import com.github.cidarosa.ms_pagamento.tests.Factory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -17,11 +19,13 @@ public class PagamentoRepositoryTest {
 
     private Long existingId;
     private Long nonExistingId;
+    private Long countTotalPagamento;
 
     @BeforeEach
     void setup() throws Exception{
         existingId = 1L;
         nonExistingId = 100L;
+        countTotalPagamento = 3L;
     }
 
     @Test
@@ -32,6 +36,20 @@ public class PagamentoRepositoryTest {
         //Assertions
         Optional<Pagamento> result = repository.findById(existingId);
         Assertions.assertFalse(result.isPresent());
+    }
+
+    @Test
+    @DisplayName("Dado parâmetros válidos e Id nulo quando chamar Criar Pagamento "
+            + "então deve instanciar um Pagamento")
+    public void givenValidParamsAndIdIsNull_whenCallCreatePagamento_thenInstantiateAPagamento(){
+
+        Pagamento pagamento = Factory.createPagamento();
+        pagamento.setId(null);
+
+        pagamento = repository.save(pagamento);
+        Assertions.assertNotNull(pagamento.getId());
+        Assertions.assertEquals(countTotalPagamento + 1, pagamento.getId());
+
     }
 
 
