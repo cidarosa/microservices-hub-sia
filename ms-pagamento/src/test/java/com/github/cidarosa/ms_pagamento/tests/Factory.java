b0129e51a276e57c09f09a2ac3e4a668c180bded
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-01 10:24:32 -0300

    test: add createPagamentoDTO on Factory, getAll - Controller

--- ms-pagamento/src/test/java/com/github/cidarosa/ms_pagamento/tests/Factory.java
+++ ms-pagamento/src/test/java/com/github/cidarosa/ms_pagamento/tests/Factory.java
@@ -1,5 +1,6 @@
 package com.github.cidarosa.ms_pagamento.tests;
 
+import com.github.cidarosa.ms_pagamento.dto.PagamentoDTO;
 import com.github.cidarosa.ms_pagamento.entity.Pagamento;
 import com.github.cidarosa.ms_pagamento.entity.Status;
 
@@ -13,5 +14,10 @@ public class Factory {
                 Status.CRIADO, 1L, 2L);
 
         return pagamento;
+    }
+
+    public static PagamentoDTO createPagamentoDTO(){
+        Pagamento pagamento = createPagamento();
+        return new PagamentoDTO(pagamento);
     }
 }
