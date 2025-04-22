166635f42e7dd6a12a219dde911d31630b11902b
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-22 11:01:55 -0300

    feat: add PedidoService method getAllPedidos

--- ms-pedido/src/main/java/com/github/cidarosa/ms_pedido/repositories/PedidoRepository.java
+++ ms-pedido/src/main/java/com/github/cidarosa/ms_pedido/repositories/PedidoRepository.java
@@ -4,5 +4,5 @@ import com.github.cidarosa.ms_pedido.entities.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-    
+
 }
