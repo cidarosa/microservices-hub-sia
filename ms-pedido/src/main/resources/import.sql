06838f105bc948649c1c6b7c1f32f9a24c3c5139
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-22 10:15:02 -0300

    feat: add carga DB, import.sql

---
+++ ms-pedido/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_pedido(nome, cpf, data, status) VALUES('Jon Snow', '123.456.789-36', '2025-05-25', 'REALIZADO');
+INSERT INTO tb_item_do_pedido(quantidade, descricao, valor_unitario, pedido_id) VALUES(2, 'Mouse sem fio Microsoft', 250.0, 1);
+INSERT INTO tb_item_do_pedido(quantidade, descricao, valor_unitario, pedido_id) VALUES(1, 'Teclado sem fio Microsoft', 290.0, 1);
+
+INSERT INTO tb_pedido(nome, cpf, data, status) VALUES('Ayra Stark', '862.654.321-21 ', '2025-05-25', 'REALIZADO');
+INSERT INTO tb_item_do_pedido(quantidade, descricao, valor_unitario, pedido_id) VALUES(1, 'Smart TV LED LG', 3599.0, 2);
+
