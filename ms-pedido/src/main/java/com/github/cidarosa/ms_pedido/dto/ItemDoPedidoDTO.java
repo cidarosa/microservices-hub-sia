2dc31b05d7161a94680bbe9743eae2f323f0cc38
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-22 10:44:39 -0300

    feat: add ItemDoPedidoDTO

---
+++ ms-pedido/src/main/java/com/github/cidarosa/ms_pedido/dto/ItemDoPedidoDTO.java
@@ -0,0 +1,31 @@
+package com.github.cidarosa.ms_pedido.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class ItemDoPedidoDTO {
+
+    private Long id;
+
+    @NotNull(message = "Quantidade requirida")
+    @Positive(message = "Quantidade deve ser um número positivo")
+    private Integer quantidade;
+
+    @NotEmpty(message = "Descrição requerido")
+    private String descricao;
+
+    @NotNull(message = "Valor unitário requerido")
+    @Positive(message = "Valor unitário deve ser um número positivo")
+    private BigDecimal valorUnitario;
+
+
+}
